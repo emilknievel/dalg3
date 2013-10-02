04ba03ecb93a038f616ca0e7407f22b0195ddd83
emilknievel <emil.vagstedt@gmail.com>
2013-10-02 11:33:18 +0200

    Initial commit.

---
+++ src/QSort.java
@@ -0,0 +1,25 @@
+public class QSort {
+  
+	/**
+	 * Quicksort the array a[] using m as cutoff to insertion sort.
+	 */
+	public static void quicksort(int[] a, int m) {
+		quicksort(a, 0, a.length - 1, m);
+	}
+
+	/**
+	 * Quicksort the subarray a[low .. high].
+	 * Uses median-of-three partitioning
+	 * and a cutoff to insertion sort of m.
+	 */
+	private static void quicksort(int[] a, int low, int high, int m) {
+		return;
+	} // dummy code
+
+	/**
+	 * Sort from a[low] to a[high] using insertion sort.
+	 */
+	private static void insertionsort(int[] a, int low, int high) {
+		return;
+	} // dummy code
+}
